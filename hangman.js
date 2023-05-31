b0fe657925bbf694b8665ad9484d37d67ecef3d9
Intuitive Harmony <j.horst77@gmail.com>
2023-05-30 22:14:48 -0600

    tally incorrect guesses

--- hangman.js
+++ hangman.js
@@ -45,13 +45,15 @@ function updateDashedWord() {
 
 // 3. Display the initial state: At the start of each round, display the current state of the hidden word to the player. Initially, all letters are hidden and replaced with underscores or dashes.
 function displayState(word) {
-  console.log(`Are you able to guess this ${secretWord.length} letter word?\n`);
+  console.log(
+    `\nAre you able to guess this ${secretWord.length} letter word?\n`
+  );
   console.log(dashedWord);
   console.log(`Letters Tried: ${guessedLetters}`);
   console.log(
     `\nYou have ${
       maxGuesses - incorrectGuesses
-    } chances to guess each letter correctly\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
+    } chances left to guess each letter correctly\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
   );
 }
 // 4. Prompt for input: Ask the player to enter a letter guess. You can use the `readline` module in Node.js to read user input from the terminal.
@@ -67,19 +69,28 @@ function playerGuess() {
     console.log(`\nYou have already tried ${guess}!`);
   } else if (guess.length === 1) {
     // input can only be one character
+    currentGuess += 1;
+    // 7. Track incorrect guesses: Maintain a counter to keep track of the number of incorrect guesses the player has made. Display the hangman figure corresponding to the number of incorrect guesses.
+    if (!secretWord.includes(guess)) {
+      console.log(`\nSorry! no ${guess}'s`);
+      incorrectGuesses += 1;
+    } else {
+      console.log(`\nNice! ${guess} is in the secret word`);
+    }
+    // Add letters to our already guessed letters array
     guessedLetters.push(guess.toLowerCase());
   } else {
     console.log(`\nYou entered: ${guess}. Please enter a single character 😎`);
   }
 }
 
-// 7. Track incorrect guesses: Maintain a counter to keep track of the number of incorrect guesses the player has made. Display the hangman figure corresponding to the number of incorrect guesses.
-
 // 8. Check win/lose conditions: After each guess, check if the player has won or lost the game. The game is won if all letters have been correctly guessed. The game is lost if the player has made a certain number of incorrect guesses.
 function checkWin() {
   // console.log(secretWord, dashedWord);
   if (secretWord === dashedWord) {
-    console.log(`You've won!`);
+    console.log(
+      `You've won!\n\nThe word was ${secretWord}.\nIt took you ${currentGuess} guesses`
+    );
     gameInProgress = false;
   }
 }
